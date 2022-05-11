4ab7b90c88c480e2bd9077ac5e168eb2bf35549e
Mikolaj Glazowski <mikolaj.glazowski@hiq.fi>
2022-05-11 15:14:29 +0200

    Add project files.

---
+++ Frends.SendGrid/Definition.cs
@@ -0,0 +1,55 @@
+﻿#pragma warning disable 1591
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Frends.SendGrid
+{
+    public class Recipients
+    {
+        public string Recipient { get; set; }
+    }
+
+    public class Attachments
+    {
+        public string ContentType { get; set; }
+        public string FileName { get; set; }
+        public string Content { get; set; }
+    }
+
+
+    /// <summary>
+    /// Parameters class usually contains parameters that are required.
+    /// </summary>
+    public class Parameters
+    {
+        /// <summary>
+        /// Something that will be repeated.
+        /// </summary>
+        public string Message { get; set; }
+    }
+
+    /// <summary>
+    /// Options class provides additional optional parameters.
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// Number of times input is repeated.
+        /// </summary>
+        public int Amount { get; set; }
+
+        /// <summary>
+        /// How repeats of the input are separated.
+        /// </summary>
+        public string Delimiter { get; set; }
+    }
+
+    public class Result
+    {
+        /// <summary>
+        /// Contains the input repeated the specified number of times.
+        /// </summary>
+        public string Replication;
+    }
+}
---
+++ Frends.SendGrid/Frends.SendGrid.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CSharp; // You can remove this if you don't need dynamic type in .NET Standard frends Tasks
+using SendGrid.Helpers.Mail;
+
+#pragma warning disable 1591
+
+namespace Frends.SendGrid
+{
+    public static class Email
+    {
+        /// <summary>
+        /// This is task
+        /// Documentation: https://github.com/CommunityHiQ/Frends.SendGrid
+        /// </summary>
+        /// <param name="input">What to repeat.</param>
+        /// <param name="options">Define if repeated multiple times. </param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>{string Replication} </returns>
+        public static async Task<Result> Send(Parameters input, [PropertyTab] Options options, CancellationToken cancellationToken)
+        {
+
+            var message = new SendGridMessage();
+
+            
+
+            return null;
+        }
+    }
+}
