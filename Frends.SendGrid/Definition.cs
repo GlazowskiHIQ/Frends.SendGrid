536e8476725b866ee01d267017d8e5c79163afae
Mikolaj Glazowski <mikolaj.glazowski@hiq.fi>
2022-05-13 15:05:08 +0200

    First commit

--- Frends.SendGrid/Definition.cs
+++ Frends.SendGrid/Definition.cs
@@ -5,11 +5,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Frends.SendGrid
 {
-    public class Recipients
-    {
-        public string Recipient { get; set; }
-    }
-
+  
     public class Attachments
     {
         public string ContentType { get; set; }
@@ -17,39 +13,21 @@ namespace Frends.SendGrid
         public string Content { get; set; }
     }
 
-
-    /// <summary>
-    /// Parameters class usually contains parameters that are required.
-    /// </summary>
     public class Parameters
     {
-        /// <summary>
-        /// Something that will be repeated.
-        /// </summary>
+       public string AuthorizationToken { get; set; }
+        public string Sender { get; set; }
+        public string[] Recipients { get; set; }
+        public string Subject { get; set; }
         public string Message { get; set; }
-    }
+        public Attachments[] Attachments { get; set; }
 
-    /// <summary>
-    /// Options class provides additional optional parameters.
-    /// </summary>
-    public class Options
-    {
-        /// <summary>
-        /// Number of times input is repeated.
-        /// </summary>
-        public int Amount { get; set; }
-
-        /// <summary>
-        /// How repeats of the input are separated.
-        /// </summary>
-        public string Delimiter { get; set; }
     }
 
     public class Result
     {
-        /// <summary>
-        /// Contains the input repeated the specified number of times.
-        /// </summary>
-        public string Replication;
+        public string StatusCode { get; set; }
+        public string Body { get; set; }
+        public string Headers { get; set; }
     }
 }
--- Frends.SendGrid/Frends.SendGrid.cs
+++ Frends.SendGrid/Frends.SendGrid.cs
@@ -3,6 +3,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CSharp; // You can remove this if you don't need dynamic type in .NET Standard frends Tasks
 using SendGrid.Helpers.Mail;
+using SendGrid;
+using System.Collections.Generic;
 
 #pragma warning disable 1591
 
@@ -11,21 +13,76 @@ namespace Frends.SendGrid
     public static class Email
     {
         /// <summary>
-        /// This is task
+        /// This task sends email via SendGrid API
         /// Documentation: https://github.com/CommunityHiQ/Frends.SendGrid
         /// </summary>
         /// <param name="input">What to repeat.</param>
         /// <param name="options">Define if repeated multiple times. </param>
         /// <param name="cancellationToken"></param>
         /// <returns>{string Replication} </returns>
-        public static async Task<Result> Send(Parameters input, [PropertyTab] Options options, CancellationToken cancellationToken)
+        public static async Task<Result> Send(Parameters input, CancellationToken cancellationToken)
         {
 
             var message = new SendGridMessage();
 
-            
+            foreach (string recipient in input.Recipients)
+            {
+                message.AddTo(new EmailAddress(recipient));
+            }
 
-            return null;
+            message.SetFrom(new EmailAddress(input.Sender));
+
+            foreach (Attachments attachment in input.Attachments)
+            {
+                message.AddAttachment(attachment.FileName, attachment.Content, attachment.ContentType);
+            };
+
+            message.MailSettings = new MailSettings()
+            {
+                BypassListManagement = new BypassListManagement()
+                {
+                    Enable = false
+                },
+                FooterSettings = new FooterSettings()
+                {
+                    Enable = false
+                },
+                SandboxMode = new SandboxMode()
+                {
+                    Enable = false
+                }
+            };
+
+            message.TrackingSettings = new TrackingSettings()
+            {
+                ClickTracking = new ClickTracking()
+                {
+                    Enable = true,
+                    EnableText = false
+                },
+                OpenTracking = new OpenTracking()
+                {
+                    Enable = true,
+                    SubstitutionTag = "%open-track%"
+                },
+                SubscriptionTracking = new SubscriptionTracking()
+                {
+                    Enable = false
+                }
+            };
+
+
+            var client = new SendGridClient(input.AuthorizationToken);
+            var response = await client.SendEmailAsync(message).ConfigureAwait(false);
+
+            var result = new Result
+            {
+                Body = response.Body.ReadAsStringAsync().Result,
+                StatusCode = response.StatusCode.ToString(),
+                Headers = response.Headers.ToString()
+            };
+
+            return result;
         }
     }
 }
